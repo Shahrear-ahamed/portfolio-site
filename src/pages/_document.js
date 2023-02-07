f460d8872ef36b5d0d3b7c9989dc5115a622cfe0
Shahrear-ahamed <its.shahrear@gmail.com>
2023-02-07 11:21:41 +0600

    project first setup:

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="Hey, I am Shahrear Ahamed, a professional and passionate, programmer and full-stack (MERN) web developer. If you want to develop or hire me, don't hesitate to contact me!"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
