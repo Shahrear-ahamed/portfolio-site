1bc3a026678628bf60db637834249fcce187aff7
Shahrear-ahamed <its.shahrear@gmail.com>
2023-02-09 18:31:56 +0600

    footer section are added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React from "react";
+import styles from "./Footer.module.css";
+import LogoImage from "../../../public/shahrear_logo.png";
+import SocialMedia from "../reusable/SocialMedia/SocialMedia";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer className={styles.footer_container}>
+      <div className={`container ${styles.footer_body}`}>
+        <Image
+          className={styles.footer_logo}
+          src={LogoImage}
+          alt="Shahrear ahamed logo"
+          width={150}
+        />
+
+        <SocialMedia />
+      </div>
+      <hr className={styles.line_break} />
+      <p className={styles.copyright}>&#169; Shahrear ahamed {year}</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer/Footer";
 import Hero from "@/components/Hero/Hero";
 import Navbar from "@/components/Navbar/Navbar";
 import Head from "next/head";
@@ -14,6 +15,7 @@ export default function Home() {
       <main>
         <Navbar />
         <Hero />
+        <Footer />
       </main>
     </>
   );
